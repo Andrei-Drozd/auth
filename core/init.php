1f5faaee4885a01bde2edc6f4f26ac22c2971314
Andrei Drozd <andrei.drozd@smartlog.loc>
2016-06-22 12:55:00 +0300

    no message

---
+++ core/init.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrei.drozd
+ * Date: 6/22/2016
+ * Time: 9:45 AM
+ */
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrei.drozd
+ * Date: 6/22/2016
+ * Time: 9:38 AM
+ */
+
