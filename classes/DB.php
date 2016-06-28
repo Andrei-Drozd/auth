74f0f39487b851c17fabf91dc5bcec6f7525eef6
Andrei Drozd <andrei.drozd@smartlog.loc>
2016-06-28 11:07:55 +0300

    no message

--- classes/DB.php
+++ classes/DB.php
@@ -17,7 +17,47 @@ class DB {
         if (!isset(self::$_instance)) {
             self::$_instance = new DB();
         }
+        return self::$_instance;
+    }
+    public function query($sql, $params = array()){
+        $this->_error = false;
+        if($this->_query = $this->_pdo->prepare($sql)) {
+            $x = 1;
+            if(count($params)) {
+                foreach ($params as $param) {
+                    $this->_query->bindValue($x, $param);
+                    $x++;
+                }
+            }
+            if($this->_query->execute()) {
+                $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                $this->_count = $this->_query->rowCount();
+            } else {
+                $this->_error = true;
+            }
+        }
+        return $this;
+    }
+    public function action($action, $table, $where = array()){
+        if(count($where) === 3) {
+            $operators = array('=', '>', '<', '<=', '>=', '!=');
+
+            $field = $where [0];
+            $operator = $where [1];
+            $value = $where [2];
+
+            if (in_array($operator, $operators)) {
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+            }
+        }
+    }
+    public function get($table, $where){
 
     }
+    public function delete($table, $where) {
 
+    }
+    public function error() {
+        return $this->_error;
+    }
 }
