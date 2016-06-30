b2e73a25968c1ced9ead96803773a63d082d741d
Andrei Drozd <andrei.drozd@smartlog.loc>
2016-06-30 10:52:14 +0300

    no message

--- classes/Validation.php
+++ classes/Validation.php
@@ -1,19 +1,15 @@
-<form action="" method="post">
-    <div class="field">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" value="" autocomplete="off">
-    </div>
-    <div class="field">
-        <label for="password">Enter password</label>
-        <input type="password" name="password" id="password">
-    </div>
-    <div class="field">
-        <label for="password_again">Repeat password</label>
-        <input type="password" name="password_again" id="password_again">
-    </div>
-    <div class="field">
-        <label for="name">Enter your name</label>
-        <input type="text" name="name" id="name">
-    </div>
-    <input type="submit" value="Register">
-</form>
+<?php
+class Validate {
+    private $_passed = false,
+            $_errors = array(),
+            $_db = null;
+
+    public function __construct() {
+        $this->_db = DB::getInstance();
+    }
+    public function check($source, $items = array()) {
+        foreach () {
+
+        }
+    }
+}
--- register.php
+++ register.php
@@ -1,1 +1,54 @@
 <?php
+require_once 'core/init.php';
+
+if(Input::exists()) {
+ //   Input::get('username');
+    $validate = new Validate();
+    $validation = $validate->check($_POST, array(
+        'username' => array(
+            'required' => true,
+            'min' => 2,
+            'max' => 20,
+            'unique' => 'users'
+        ),
+        'password' => array(
+            'required' => true,
+            'min' => 6
+        ),
+        'password_again' => array(
+            'required' => true,
+            'matches' => 'password'
+        ),
+        'name' => array(
+            'required' => true,
+            'min' => 2,
+            'max' => 50
+        )
+    ));
+    if($validation->passed()) {
+
+    }
+}
+
+
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password">Enter password</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div class="field">
+        <label for="password_again">Repeat password</label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+    <div class="field">
+        <label for="name">Enter your name</label>
+        <input type="text" name="name" value="" id="name">
+    </div>
+    <input type="submit" value="Register">
+</form>
